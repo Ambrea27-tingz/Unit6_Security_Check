fc3cc42454f0f9e6b9128f2d02e2ec8a7e311e2a
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-06-13 22:33:41 -0400

    Created a dictionary of usernames and passwords

--- Lab6_awilliams#1.py
+++ Lab6_awilliams#1.py
@@ -8,3 +8,46 @@
    assigns a security level, and greets the user upon successful login. 
    Guests can log in with limited access using “guest”/“guest”
    """
+
+# Step 1: Create a dictionary of usernames and passwords
+user_files = {
+    "Sally": {
+        "password": "PrettyInPink@614"
+    },
+
+    "Paul": {
+        "password": "TomandJerry@123"
+    },
+
+    "Sabrina": {
+        "password": "SabrinaTheTeenageWitch@456"
+    },
+    
+    "guest": {
+        "password": "guest"
+    },
+}
+
+# Step 2: Define security levels
+
+
+
+
+
+
+# Step 3: Ask the user for their username
+
+
+
+
+
+
+# Step 4: Check if the username exists
+
+
+
+
+
+# Step 5: Ask for the password (since username exists)
+
+
