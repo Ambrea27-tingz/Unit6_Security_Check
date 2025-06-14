1295674581b2206a59a94e4d6011557ebd35549f
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-06-13 22:20:22 -0400

    Started file with a brief description of the code.

--- Lab6_awilliams#1.py
+++ Lab6_awilliams#1.py
@@ -2,7 +2,9 @@
    
    Date: 06/07/2025
 
-   Unit 3: Lab 3
+   Unit 6: Lab 6 - Security Password Checker
 
-   Description:
+   Description: login program that checks a username and password from a dictionary, 
+   assigns a security level, and greets the user upon successful login. 
+   Guests can log in with limited access using “guest”/“guest”
    """
